433edd2b6f63e315eb530093c151c9d61384cb0a
David <48280799+gdcho@users.noreply.github.com>
2023-09-19 22:26:03 -0700

    Time: 50 ms (15.78%), Space: 16.3 MB (73.02%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,33 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        carry = 0
-        out = []
-        
-        a, b = a[::-1], b[::-1]
-        
-        if len(b) > len(a):
+        if len(a) < len(b):
             a, b = b, a
-        
-        for i in range(len(a)):
-            sum_val = carry
-            
-            if i < len(a):
-                sum_val += int(a[i])
-            
-            if i < len(b):
-                sum_val += int(b[i])
-            
-            if sum_val == 2:
-                out.append('0')
-                carry = 1
-            elif sum_val == 3:
-                out.append('1')
-                carry = 1
-            else:
-                out.append(str(sum_val))
-                carry = 0
-        
+        ans = ""
+        carry = 0
+        a, b = a[::-1], b[::-1]
+        l1, l2 = len(a), len(b)
+        for i in range(l1):
+            t = carry
+            t += int(a[i])
+            if i < l2:
+                t += int(b[i])
+            t, carry = t % 2, t // 2
+            ans += str(t)
         if carry:
-            out.append('1')
-        
-        return ''.join(out[::-1])
+            ans += str(carry)
+        return ans[::-1]
