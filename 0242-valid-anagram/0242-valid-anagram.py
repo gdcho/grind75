0337c22661ac84cbe38d380fd00c573efa021956
David <48280799+gdcho@users.noreply.github.com>
2023-09-24 20:25:03 -0700

    Time: 40 ms (36.36%), Space: 14.5 MB (47.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,9 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        sCount = {}
-        tCount = {}
-
-        for i in range(len(s)):
-            sCount[s[i]] = 1 + sCount.get(s[i], 0)
-            tCount[t[i]] = 1 + tCount.get(t[i], 0)
-        for c in sCount:
-            if sCount[c] != tCount.get(c, 0):
-                return False
-
-        return True
-
-        ## return sorted(s) == sorted(t)
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
+        
