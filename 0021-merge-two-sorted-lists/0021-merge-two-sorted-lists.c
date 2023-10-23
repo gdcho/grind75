efac308b288e0c63a5ecba3083dbbee844f92aea
David <48280799+gdcho@users.noreply.github.com>
2023-10-23 02:08:39 -0700

    Time: 6 ms (20.85%), Space: 6.9 MB (18.01%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -1,28 +1,55 @@
-struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-    // Create a dummy node.
-    struct ListNode dummy;
-    dummy.val = 0;
-    dummy.next = NULL;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+// iterative solution
+// struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+//     struct ListNode dummy;
+//     dummy.val = 0;
+//     dummy.next = NULL;
     
-    struct ListNode* current = &dummy;
+//     struct ListNode* current = &dummy;
 
-    while(list1 != NULL && list2 != NULL){
-        if(list1->val < list2->val){
-            current->next = list1;
-            list1 = list1->next;
-        } else {
-            current->next = list2;
-            list2 = list2->next;
-        }
-        current = current->next;
+//     while(list1 != NULL && list2 != NULL){
+//         if(list1->val < list2->val){
+//             current->next = list1;
+//             list1 = list1->next;
+//         } else {
+//             current->next = list2;
+//             list2 = list2->next;
+//         }
+//         current = current->next;
+//     }
+
+//     if(list1 == NULL){
+//         current->next = list2;
+//     } else {
+//         current->next = list1;
+//     }
+
+//     return dummy.next;
+// }
+
+// recursive solution
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    // Base cases
+    if (list1 == NULL) {
+        return list2;
+    }
+    if (list2 == NULL) {
+        return list1;
     }
 
-    // At this point, one of the lists is empty.
-    if(list1 != NULL){
-        current->next = list1;
+    // Recursive case
+    if (list1->val < list2->val) {
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
     } else {
-        current->next = list2;
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
     }
-
-    return dummy.next;
 }
